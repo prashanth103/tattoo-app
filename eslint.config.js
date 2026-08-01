b6746c976b438657dca567590a666d4f401c2e30
prashanth103 <cheeralaprashanth10@gmail.com>
2026-08-01 12:55:42 +0530

    eslint, prettier added

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist"],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      globals: globals.browser,
+    },
+
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier,
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
+      "prettier/prettier": "warn",
+    },
+  },
+
+  eslintConfigPrettier
+);
